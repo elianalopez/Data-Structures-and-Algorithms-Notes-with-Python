c777a99c65dca08b4976e902e8388cf8e01936db
elianalopez <39322110+elianalopez@users.noreply.github.com>
2020-11-24 17:35:52 -0500

    Update Stack.py

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -7,17 +7,35 @@ class Stack:
         self.item.append(item) #adds an item to the stack
 
     def pop(self):
-        return self.items.pop() #removes and returns item
+        if len(self.items) > 0:
+            return self.items.pop() #removes and returns item
+        elif len(self.items) == 0:
+            return None
 
     def peek(self):
-        return self.items[-1]
-        #returns to the top of the stack, but does not remove it
-        #return(self.items[len(self.items)-1])
+        #make an if-else on how to return it
+        if len(self.items) > 0:
+            return self.items[-1]
+            #returns to the top of the stack, but does not remove it
+            #return(self.items[len(self.items)-1]) an alternative
+        elif len(self.items) == 0:
+            return None
+            
 
     def isEmpty(self):
         return self.items == []
-        #return self.size() == 0
+        #an alternative: return self.size() == 0
 
     def size(self):
         return len(self.items)
-        
+ 
+    #for i in a list would print each item in the list. 
+    def __str__(self):
+        s = "["
+        for i in self.items:
+            s +=  str(i)
+            #this would create a space for each item printed within the list
+            if self.items.index(i) !=(len(self.items)-1):
+                s += ", "  
+        s +=   "]" 
+        return s
